b5bad17baf0f32044fc17af2ba5562de89f06ba5
Peter Backx <peter.backx@gmail.com>
2019-05-17 22:39:15 +0200

    mailing signup

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
-/**
- * Created by fabiomadeira on 25/02/15.
- */
+function showPopup() {
+	console.log('pop')
+
+	window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us1.list-manage.com","uuid":"dfef2582094d2c4bfe9f2a0f0","lid":"a04d8917e6","uniqueMethods":true}) })
+	//unsetting the cookie
+	document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";                  
+	document.cookie = "MCPopupSubscribed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
+
 // jQuery for page scrolling feature
 jQuery(document).ready(function(e) {
     e(".scroll").click(function(t) {
@@ -9,6 +15,5 @@ jQuery(document).ready(function(e) {
             scrollTop: e(this.hash).offset().top
         }, 1e3)
     })
+    e(".btn .btn-warning").click(showPopup);
 });
-
-
